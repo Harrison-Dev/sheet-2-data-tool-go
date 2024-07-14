9c8c6ece9f19f5b2cac63b809ccac200698d6967
Harrison-Dev <hao0628@gmail.com>
2024-07-14 23:12:17 +0800

    simplify build script

--- scripts/build_macos.sh
+++ scripts/build_macos.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o data-generator .
+go build -o data-generator
--- scripts/build_windows.bat
+++ scripts/build_windows.bat
@@ -1,5 +1,2 @@
 @echo off
-SET CGO_ENABLED=0
-SET GOOS=windows
-SET GOARCH=amd64
 go build -o data-generator.exe .
